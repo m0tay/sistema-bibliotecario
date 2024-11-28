eef430158b7e63263fac9d1287fe01cf5dd6ef84
Douglas Lobo <douglascnlobo@icloud.com>
2024-11-28 18:31:41 +0000

    update: adicionado o exemplo do acesso à tabela usuários

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from helpers import database as db
 from models.books import Book
-
+from models.users import User
+\
 if __name__ == "__main__":
 
     # limpando qualquer tabela
     # db.clear_table(Book.table_name)
+    # db.clear_table(User.table_name)
 
     # "dropando" eliminando a tabela
     db.drop_table(Book.table_name)
@@ -23,13 +25,16 @@ if __name__ == "__main__":
              synopsis="A romantic novel exploring themes of love and social standing.", publisher="T. Egerton, Whitehall", genres="Romance|Clássicos|Drama", isbn="9789897391019")
 
     # imprimindo uma lista dos livros na tabela books
-    print("Livros:\n", "\n\t".join(
-        [str(book) for book in Book.browse()]) if Book.browsable() else "não há livros")
+    if books := Book.browse():
+        print("Livros:\n", "\n\t".join(
+            [str(book) for book in books]))
+    else:
+        print("não há livros")
 
     # retorna o livro com base no id referido
     book = Book.read(3)
     print(f"\n\nRead book: {book}")
-    
+
     # atualiza os campos passados do livro referido
     Book.edit(book_id=3, authors="Lee Harper",
               genres="Drama|Suspense", published_date="2024-11-22")
@@ -44,7 +49,7 @@ if __name__ == "__main__":
 
     # atualiza os campos passados do livro referido
     Book.edit(book_id=2, authors="Leo Tolstoyi", title="War and Peace")
-
+    
 
     # retorna o livro com base no id referido
     book = Book.read(2)
@@ -54,12 +59,33 @@ if __name__ == "__main__":
 
     # retorna o livro com base no id referido
     book = Book.read(2)
-    
+
     # deleta o livro referido da tabela
     Book.delete(book_id=book.id)
 
     print("\n\n")
 
     # imprimindo uma lista dos livros na tabela books
-    print("Livros:\n", "\n\t".join(
-        [str(book) for book in Book.browse()]) if Book.browsable() else "não há livros")
+    if books := Book.browse():
+        print("Livros:\n", "\n\t".join(
+            [str(book) for book in books]))
+    else:
+        print("não há livros")
+        
+        
+    # Interface ao banco de dados
+    db.drop_table(User.table_name)
+    db.create_table(User.table_name, db.table_fields(User))
+    
+    # Interface ao modelo
+    User.add(name="Nícolas Alvez", register_date="2024-11-22")
+    User.add(name="Tiago Novo", register_date="2024-11-20")
+    
+    if users := User.browse():
+        print("Usuários:\n", "\n\t".join(
+            [str(user) for user in users]))
+    else:
+        print("não há usuários")
+        
+    
+        
