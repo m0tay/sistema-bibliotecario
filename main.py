5de42fc73e03a3a6ef2633d0a7ef87eb02862429
Douglas Lobo <douglascnlobo@icloud.com>
2024-11-29 17:23:58 +0000

    atualizado o exemplo de como usar os livros, usuários e empréstimos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from helpers import database as db
 from models.books import Book
+from models.lendings import Lending
 from models.users import User
 \
 if __name__ == "__main__":
@@ -23,6 +24,21 @@ if __name__ == "__main__":
 
     Book.add(title="Pride and Prejudice", authors="Jane Austen", published_date="1813-01-28",
              synopsis="A romantic novel exploring themes of love and social standing.", publisher="T. Egerton, Whitehall", genres="Romance|Clássicos|Drama", isbn="9789897391019", pages=623, sub_title="a novel of two lovers")
+
+    Book.add(
+        title="Naturalis Historia",
+        authors="Plínio, o Velho",
+        published_date="77",
+        synopsis=(
+            "Uma enciclopédia monumental que abrange uma ampla variedade de temas, "
+            "como astronomia, geografia, zoologia, botânica, medicina, arte e muito mais."
+        ),
+        publisher="Imperial Rome",
+        genres="Ciência|Enciclopédia|História",
+        isbn="9780521202244",
+        pages=2564,
+        sub_title="A Grande Enciclopédia do Mundo Antigo"
+    )
 
     # imprimindo uma lista dos livros na tabela books
     if books := Book.browse():
@@ -88,3 +104,47 @@ if __name__ == "__main__":
             [str(user) for user in users]))
     else:
         print("não há usuários")
+
+    db.drop_table(Lending.table_name)
+    db.create_table(Lending.table_name, db.table_fields(Lending))
+
+    book = Book.read(4)
+    user = User.read(2)
+
+    Lending.add(user_id=user.id, book_id=book.id,
+                from_date="2022-10-28", to_date="2022-11-12")
+
+    book = Book.read(3)
+    user = User.read(2)
+
+    Lending.add(user_id=user.id, book_id=book.id,
+                from_date="2024-11-28", to_date="2024-12-12")
+
+    book = Book.read(1)
+
+    Lending.add(user_id=user.id, book_id=book.id,
+                from_date="2024-11-28", to_date="2024-12-12")
+
+    Lending.edit(2, to_date="2024-12-26")
+
+    book = Book.read(4)
+    user = User.read(1)
+
+    Lending.add(user_id=user.id, book_id=book.id,
+                from_date="2024-11-29", to_date="2024-12-13")
+
+    for user in User.browse():
+        for lending in Lending.from_user(user.id):
+            name_of_user_from_lending: str = User.read(lending.user_id).name
+            print(f"Empréstimo de {name_of_user_from_lending}:", "\n\t".join(
+                [str(lending)]))
+
+    for book in Book.browse():
+        for lending in Lending.from_book(book.id):
+            title_of_book_from_lending: str = Book.read(lending.book_id).title
+            print(f"Empréstimo do livro {title_of_book_from_lending}:", "\n\t".join(
+                [str(lending)]))
+
+    if lendings := Lending.browse():
+        print("Empréstimos:\n", "\n\t".join(
+            [str(lending) for lending in lendings]))
