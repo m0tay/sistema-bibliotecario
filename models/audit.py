e5b95e5fd06ce9ff4dca65d2bc684ff72be9dcc6
Douglas Lobo <douglascnlobo@icloud.com>
2024-12-05 19:37:17 +0000

    atualizados os modelos para serem tipos e não mais classes com métodos

--- models/audit.py
+++ models/audit.py
@@ -12,47 +12,9 @@ class Audit:
     table_name = "audits"
 
     # Campos da tabela
-    id: int
+    id:Optional [int] = None
     _: KW_ONLY
     # falta definir
 
     def __str__(self):
         ...
-
-    @classmethod
-    def browse(cls) -> Optional[list["Audit"]]:
-        """
-        Retorna todos os registros da tabela `audits`.
-        """
-        pass
-
-    @classmethod
-    def read(cls, audit_id: int) -> Optional["Audit"]:
-        """
-        Retorna um único registro de auditoria através do id.
-        """
-        pass
-
-    @classmethod
-    def edit(cls, audit_id: int, **fields) -> Optional["Audit"]:
-        """
-        Atualiza o registro especificado.
-
-        Campos da tabela
-        `falta definir`
-        """
-        pass
-
-    @classmethod
-    def add(cls, **fields) -> None:
-        """
-        Insere um novo registro de auditoria na tabela `audits`.
-        """
-        pass
-
-    @staticmethod
-    def delete(audit_id: int) -> None:
-        """
-        Deleta um registro de auditoria da tabela `audits` através do id.
-        """
-        pass
--- models/books.py
+++ models/books.py
@@ -1,11 +1,6 @@
-from dataclasses import dataclass, KW_ONLY
-from datetime import datetime
+from dataclasses import KW_ONLY, dataclass
+from datetime import date
 from typing import Optional
-from helpers import database as db
-
-# Géneros:
-genres_list: list[str] = ['Romance', 'Mistério', 'Fantasia', 'Ficção Científica', 'Aventura', 'História', 'Terror', 'Clássicos', 'Poesia', 'Drama', 'Infantojuvenil', 'Autoajuda', 'Religião',
-                          'Filosofia', 'Humor', 'Biografia', 'Autobiografia', 'Suspense', 'Thriller', 'Contos', 'Distopia', 'Utopia', 'Realismo', 'Épico', 'Western', 'Policial', 'Chick-lit', 'Jovem Adulto', 'Guerra', 'Ensaios']
 
 
 @dataclass
@@ -13,18 +8,18 @@ class Book:
     table_name = "books"
 
     # Campos da tabela
-    id: int
+    id: Optional[int] = None
     _: KW_ONLY
     title: str
     sub_title: Optional[str]
     authors: str
-    published_date: datetime
+    published_date: date
     synopsis: str
     publisher: str
     isbn: Optional[str]
     genres: str
     pages: Optional[int]
-    
+
     def __str__(self):
         return (f"\n\t{'Book ID:':.<24}{self.id}\n"
                 f"\t{'Title:':.<24}{self.title}\n"
@@ -36,62 +31,3 @@ class Book:
                 f"\t{'Genres:':.<24}{self.genres}\n"
                 f"\t{'Synopsis:':.<24}{self.synopsis}\n"
                 f"\t{'Pages:':.<24}{self.pages}\n")
-
-    def genres() -> list[str]:
-        """
-        Método estático que retorna os todos géneros possíveis `Book`.
-        """
-        return genres_list
-
-    @classmethod
-    def browse(cls) -> Optional[list["Book"]]:
-        """
-        Retorna todos os livros da tabela `books`.
-        """
-        rows = db.browse(Book.table_name, db.table_fields(
-            cls, return_field_names=True, return_field_id=True))
-        return [Book(**row) for row in rows] if rows else None
-
-    @classmethod
-    def read(cls, book_id: int) -> Optional["Book"]:
-        """
-        Retorna um único livro através do id.
-        """
-        result = db.read(Book.table_name, db.table_fields(
-            cls, return_field_names=True, return_field_id=True), f"id = {book_id}")
-        return Book(**result) if result else None
-
-    @classmethod
-    def edit(cls, book_id: int, **fields) -> Optional["Book"]:
-        """
-        Atualiza o registo especificado.
-
-        Campos da tabela
-        - `title`: Armazena o título da obra.
-        - `authors`: Lista de autores da obra.
-        - `published_date`: Data de publicação da obra.
-        - `synopsis`: Resumo ou descrição da obra.
-        - `publisher`: Editora responsável pela publicação.
-        - `genres`: Gêneros aos quais a obra pertence.
-        """
-        condition: str = f"id = {book_id}"
-
-        db.validate_fields(cls, fields)
-
-        db.edit(Book.table_name, fields, condition)
-
-    @classmethod
-    def add(cls, **fields) -> None:
-        """
-        Insere um livro à tabela `books`.
-        """
-        db.validate_fields(cls, fields)
-
-        db.add(Book.table_name, fields)
-
-    @staticmethod
-    def delete(book_id: int) -> None:
-        """
-        Deleta um livro da tabela `books` através do id.
-        """
-        db.delete(Book.table_name, f"id = {book_id}")
